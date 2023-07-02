e42e27c66c40f1943aa488d7cd2c658777a43d61
Jun <kumejun2000@gmail.com>
2023-07-02 23:11:52 +0900

    main.goの追加

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+var store = sessions.NewCookieStore([]byte("secret-key"))
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+	"user3": "password3",
+}
+
+func main() {
+	fmt.Println("AccessURL: http://localhost:8080/")
+	http.HandleFunc("/", loginHandler)
+	http.HandleFunc("/dashboard", dashboardHandler)
+	http.HandleFunc("/logout", logoutHandler)
+
+	log.Println("Server started on localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if pass, ok := users[username]; ok && pass == password {
+			session, _ := store.Get(r, "session")
+			session.Values["username"] = username
+			session.Save(r, w)
+			http.Redirect(w, r, "/dashboard", http.StatusFound)
+		} else {
+			renderTemplate(w, "login.html", "Invalid username or password")
+		}
+	} else {
+		renderTemplate(w, "login.html", nil)
+	}
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	username, ok := session.Values["username"].(string)
+	if !ok || username == "" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "dashboard.html", username)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	session.Values["username"] = ""
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	tmpl = fmt.Sprintf("templates/%s", tmpl)
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
