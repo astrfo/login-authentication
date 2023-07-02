0f62be20276cea2667dcad3e216bc9da5f48e1b4
Jun <kumejun2000@gmail.com>
2023-07-03 00:06:00 +0900

    login-validation.jsが呼ばれるように修正

--- templates/login-validation.js
+++ templates/login-validation.js
@@ -1,7 +1,9 @@
-function validateForm() {
+function loginValidateForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    
+    console.log("Username value:", document.getElementById("username").value.trim());
+    console.log("Password value:", document.getElementById("password").value.trim());
+
     if (username.trim() === "" || password.trim() === "") {
         alert("Please enter both username and password.");
         return false;
